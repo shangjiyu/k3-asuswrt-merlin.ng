233662fc639ae29d904ce1a6ad6df33b0274c412
Rokis <ghost1988102@gmail.com>
2020-07-15 01:51:24 +0800

    httpd: remove downgrade limit
    Signed-off-by: Rokis <ghost1988102@gmail.com>

--- release/src/router/httpd/web.c
+++ release/src/router/httpd/web.c
@@ -14642,6 +14642,8 @@ do_upgrade_post(char *url, FILE *stream, int len, char *boundary)
 
 	upgrade_err = check_imagefile(upload_fifo);
 
+	if (upgrade_err == 2)
+		nvram_set_int("reboot_time", nvram_get_int("reboot_time")+30);
 	if (upgrade_err) /* 0: legal image, 1: illegal image 2: new trx format validation failure */
 		goto err;
 
@@ -14682,7 +14684,7 @@ do_upgrade_cgi(char *url, FILE *stream)
                 return;
         }
 
-	if (upgrade_err == 0)
+	if (upgrade_err == 0 || upgrade_err == 2)
 	{
 #if defined(RTCONFIG_DSL) && defined(RTCONFIG_RALINK)
 		int ret_val_comp;
@@ -14728,6 +14730,8 @@ do_upgrade_cgi(char *url, FILE *stream)
 		system("ls -al /tmp");
 		system(SINGLE_IMAGE_SCRIPT);
 #endif
+		if (upgrade_err == 2 && !err)
+			system("nvram erase");
 	}
 	else
 	{
