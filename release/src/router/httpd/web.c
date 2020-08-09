f6e7bfb0aa15d9455a3ad6f23e47736f6dbb6d23
Rokis <ghost1988102@gmail.com>
2020-08-09 18:09:20 +0800

    httpd: fix backup & restore jffs funtion.
    Signed-off-by: Rokis <ghost1988102@gmail.com>

--- release/src/router/httpd/web.c
+++ release/src/router/httpd/web.c
@@ -33036,4 +33036,48 @@ err:
 
 	fcntl(fileno(stream), F_SETOWN, -ret);
 }
-#endif // (defined(RTCONFIG_JFFS2) || defined(RTCONFIG_BRCM_NAND_JFFS2)
+#endif // (defined(RTCONFIG_JFFS2) || defined(RTCONFIG_BRCM_NAND_JFFS2 || defined(RTCONFIG_UBIFS))
+
+#ifdef RTCONFIG_DNSPRIVACY
+int
+ej_get_dnsprivacy_presets(int eid, webs_t wp, int argc, char_t **argv)
+{
+	FILE *fp;
+	char buf[256], *type, *datafile, *ptr, *item, *lsep, *fsep;
+	int ret = 0;
+
+	if (ejArgs(argc, argv, "%s", &type) < 1) {
+		websError(wp, 400, "Insufficient args\n");
+		return -1;
+	}
+
+	if (!strcmp(type, "dot"))
+		datafile = "/rom/dot-servers.dat";
+	else {
+		websError(wp, 400, "Invalid argument\n");
+		return -1;
+	}
+
+	if (!(fp = fopen(datafile, "r")))
+		return 0;
+
+	for (lsep = ""; (ptr = fgets(buf, sizeof(buf), fp)) != NULL;) {
+		buf[sizeof(buf) - 1] = '\0';
+		ptr = strsep(&ptr, "#\n");
+		if (*ptr == '\0')
+			continue;
+
+		ret += websWrite(wp, "%s[", lsep);
+		for (fsep = ""; (item = strsep(&ptr, ",")) != NULL;) {
+			ret += websWrite(wp, "%s\"%s\"", fsep, item);
+			fsep = ",";
+		}
+		ret += websWrite(wp, "]");
+		lsep = ",";
+	}
+	fclose(fp);
+
+	return ret;
+}
+#endif
+
